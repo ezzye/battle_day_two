d43b6faaeb8c35758f477def0a1251bafc128a20
thisdotrob <thisdotrob@gmail.com>
2015-10-28 17:02:02 +0000

    Make Game class take two Players on init

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,20 @@
 class Game
 
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
   def attack player
     player.reduce_hp
   end
+
+  def players
+    [player1, player2]
+  end
+
+  private
+
+  attr_reader :player1, :player2
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,18 @@
 require 'game.rb'
 
 describe Game do
-  subject(:game) {Game.new }
-  let(:player1) {double(:player1, :reduce_hp => nil)}
-  let(:player2) {double(:player2)}
+  subject(:game) { Game.new(player1, player2) }
+  let(:player1) { double(:player1, :reduce_hp => nil) }
+  let(:player2) { double(:player2) }
 
-  context 'when player actacks another player'
-    it '#attack' do
+  context '#initialize' do
+    it 'creates two Player instances' do
+      expect(game.players).to eq [player1, player2]
+    end
+  end
+
+  context '#attack'
+    it 'calls #reduce_hp on the Player' do
       expect(player1).to receive(:reduce_hp)
       game.attack(player1)
     end
