7502939fae5b366c62f988b10443dd3e49ff3e05
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-10-28 10:26:45 +0000

    updated feature test

--- spec/features/enter_player_names_spec.rb
+++ spec/features/enter_player_names_spec.rb
@@ -1,22 +1,37 @@
-require 'capybara'
-require 'selenium-webdriver'
-include Capybara::DSL
-Capybara.default_driver = :selenium
+
+# As two Players,
+# So we can play a personalised game of Battle,
+# We want to Start a fight by entering our names and seeing them
+
+
+feature 'Enter user names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :Player_one, with: 'Rafael'
+    fill_in :Player_two, with: 'Ezzy'
+    click_button 'Submit'
+    expect(page).to have_content 'Player one: Rafael Vs Player two: Ezzy'
+  end
+end
+
+
+
+
 
 # goto form
 
-visit('http://localhost:4567/')
+# visit('http://localhost:4567/')
 
 # fill in player one name
 
-fill_in('Player_one', :with => 'Rafael')
+# fill_in('Player_one', :with => 'Rafael')
 
 # fill in played two name
 
-fill_in('Player_two', :with => 'Ezzy')
+# fill_in('Player_two', :with => 'Ezzy')
 
 # submit form
 
-click_button('Submit')
+# click_button('Submit')
 
 # see names on screen
