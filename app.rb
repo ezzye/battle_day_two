ec5e588b66f1b0dc271d02703813594b8b6eb41a
thisdotrob <thisdotrob@gmail.com>
2015-10-28 10:55:21 +0000

    Refactor to use redirect loop for /play

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
-    @Player_one = params[:Player_one]
-    @Player_two = params[:Player_two]
-    erb(:play)
+    session[:player1] = params[:Player_one]
+    session[:player2] = params[:Player_two]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player_one = session[:player1]
+    @Player_two = session[:player2]
+    erb :play
   end
 
   # start the server if ruby file executed directly
